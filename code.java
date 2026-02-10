5ccb341264d15927098c42799b90d1fa187f38f6
Yash vardhan vats <yashvardhanvats2004@gmail.com>
2026-02-10 10:27:58 +0530

    Add reverse number solution

---
+++ code.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int reverseNumber(int num) {
+        int reversed = 0;
+        int temp = Math.abs(num);
+        
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+        
+        return num < 0 ? -reversed : reversed;
+    }
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        int num1 = 12345;
+        System.out.println("Original: " + num1 + ", Reversed: " + solution.reverseNumber(num1));
+        
+        int num2 = -9876;
+        System.out.println("Original: " + num2 + ", Reversed: " + solution.reverseNumber(num2));
+        
+        int num3 = 1000;
+        System.out.println("Original: " + num3 + ", Reversed: " + solution.reverseNumber(num3));
+    }
+}
